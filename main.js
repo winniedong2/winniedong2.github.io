84f15c408173aba9283e1af38fd8bc2aadcb3599
winniedong2 <winniedong2@gmail.com>
2016-11-03 15:55:45 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function math() {
+	var answer = prompt("Enter 1+1:");
+	if (answer == 2){
+  	confirm("TEN POINTS FOR SLYTHERIN! YOU GO DRACO! Your answer was " + answer);
+  }
+ else {
+ 	confirm("NEGATIVE TEN THOUSAND POINTS FOR GRYFFINDOR! go die in a ditch harry pooter")
+ 
+}
+ }
